bb1351dbf55758bb70775b9248692d3efc8214a0
Abhishek Kumar <abhishekhchd@gmail.com>
2025-05-24 15:18:35 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to My First React App!</h1>
+      <h2>Hi, I am Abhishek Kumar 👋</h2>
+      <p>This is my live React project.</p>
+    </div>
+  );
+}
+
+export default App;
